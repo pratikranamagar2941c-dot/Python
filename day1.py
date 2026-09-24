3241b2c738785247f8f06881c4f9299b3114fc3b
[ItsPatrick] <[pratikranamagar2941c@gmail.com]>
2026-09-24 16:13:41 +0545

    Added project files

---
+++ day1.py
@@ -0,0 +1,25 @@
+
+print("hello world")
+
+a=3
+b=7
+result=a+b
+
+if 5>2:
+  print("five is greater than two!")
+
+print("i am pratik rana")
+
+print(7)
+
+print(9 + 4)
+
+print(89*7)
+
+print("i am", 57 ,"years old.")
+
+age = 9
+
+print (age)
+
+my_var="Pratik"
---
+++ day2.py
@@ -0,0 +1,55 @@
+print(abc)
+print(3)
+abc=3
+print(abc)    #prints the value of abc
+print("abc")    #prints the letter abc
+
+
+
+# WAP to add two numbers
+
+a=1
+b=2
+a+b
+print(a+b)    #prints the output (3)
+print("a+b")  #prints the output (a+b)
+
+
+
+# WAP to multiply two numbers
+a=3
+b=4
+a*b
+result=a*b
+print(a*b)       #prints 12
+print(result)    #prints 12
+
+x,y,z="orange" , "Banana" , "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "orange"
+print(x)
+print(y)
+print(z)
+
+x= "python"
+y= "is"
+z= "awesome"
+print(x+y+z)
+
+
+x= "python"
+y= "is"
+z= "awesome"
+print(x,y,z)
+
+x=int(2.01)
+print(x)
+
+x=float(2.😎
+print(x)
+
+y=float(1.7)
+print(y)
